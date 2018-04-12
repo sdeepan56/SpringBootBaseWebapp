846946daf523e52363a406cf3baaae6745d0b55e
dsubramani <dsubramani@rxbenefits.com>
2018-04-12 15:25:45 -0500

    OWASP

--- src/main/java/netgloo/Application.java
+++ src/main/java/netgloo/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        System.getProperties().put( "server.port", 8181 );  //8181 port is set here Configuration.
+        System.getProperties().put( "server.port", 8181 );  //8181 port is set here.
 
         SpringApplication.run(Application.class, args);
     }
