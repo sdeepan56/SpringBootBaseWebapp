4127906d0e305aac992a2d064061f33115fe71f1
dsubramani <dsubramani@rxbenefits.com>
2018-04-12 11:57:50 -0500

    port change 8181

---
+++ src/main/java/netgloo/Application.java
@@ -0,0 +1,15 @@
+package netgloo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        System.getProperties().put( "server.port", 8181 );  //8181 port is set here
+
+        SpringApplication.run(Application.class, args);
+    }
+
+}
